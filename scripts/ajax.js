c3e29ec67c76f34a8e7e696454c898ce9355254e
bcoan2 <122556056+bcoan2@users.noreply.github.com>
2023-04-10 18:07:43 -0500

    Add files via upload

---
+++ scripts/ajax.js
@@ -0,0 +1,42 @@
+// $(function() {
+
+
+$('#btnLoadData').click(function() {
+    console.log("clicked");
+
+    // url = file
+    let jsonURL = "demo.json";
+
+    $.ajax({
+        url: jsonURL,
+        dataType: "json",
+        success: function(data) {
+            //can log either the entire data or invoke specific properties data.zipCode...
+            console.log(data.firstName);
+
+            //loads first name into my fname box
+            $("#noSpaces").val(data.firstName);
+            console.log(data.language);
+
+            $('input[type=checkbox]').each(function() {
+                if (data.language === $(this).val()) {
+                    $(this).prop('checked', true)
+                    console.log($(this));
+                }
+
+            });
+            //console.log (sList);
+
+            $.each(data, function(key, val) {
+                //step through results
+                console.log(key, val);
+                //get each data item
+                //put it in the right place
+                $(`#${key}`).val(val);
+
+            });
+        }
+    });
+});
+
+
