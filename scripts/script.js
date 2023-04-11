6d5534eb8433ffea648d8dc629276d537d4487e5
jacksonthompson1 <122913401+jacksonthompson1@users.noreply.github.com>
2023-04-11 11:42:13 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,36 @@
-let petNames = [
-    ["Dog", ["German Shepherd", "Bulldog", "DOGE DOG", "Husky"]],
-    ["Cat", ["Siamese", "Persian", "Shorthair", "Calico"]],
-    ["Horse", ["Friesian", "Mustang", "American Quarter Horse"]],
-    ["Snake", ["Python", "Anaconda", "Rattlesnake"]],
-    ["Rabbit", ["Flemish", "Dwarf Hotot"]],
-    ["Fish", ["Bass", "Catfish", "Betta"]],
-    ["Turtle", ["Western Painted", "Tortoise"]],
-    ["Bird", ["Parakeet", "Crow"]],
-    ["Camel", ["Dromedary", "Bactrian"]],
+let boardNames = [
+    ["Burton", ["Flat top", "Directional Flat Top", "Camber", "PurePop"]],
+    ["Arbor", ["Flat top", "Directional Flat Top", "Camber", "PurePop"]],
+    ["Capita", ["Flat top", "Directional Flat Top", "Camber", "PurePop"]],
+    
 ];
 
-//when user selects type of pet...load in breeds of pets from array
-$("#petKind").on("change", function(e) {
-    //enables the pet dropdown
-    $("#petName").prop("disabled", false);
+//when user selects type of board...load in brands and types of boards from array
+$("#boardKind").on("change", function(e) {
+    //enables the board dropdown
+    $("#boardName").prop("disabled", false);
 
     let inputVal = this.value;
 
-    //loop though array of pet types
-    $.each(petNames, function(key, value) {
-        //match pet kind to user selected
+    //loop though array of board types
+    $.each(boardNames, function(key, value) {
+        //match board type to user selected
         if (inputVal === value[0]) {
             $.each(value, function(nestKey, nestValue) {
 
                 switch (nestKey) {
                     case 0:
-                        $("label[for=petName]").text(nestValue);
-                        $("#petName").empty();
-                        $("#petName").append(
-                            $("<option>").text(`Select a ${nestValue} breed`)
+                        $("label[for=boardName]").text(nestValue);
+                        $("#boardName").empty();
+                        $("#boardName").append(
+                            $("<option>").text(`Select a ${nestValue} type`)
                         );
                         break;
                     case 1:
                         $.each(nestValue, function(nameKey, nameValue) {
                             console.log(nameKey, nameValue);
 
-                            $("#petName").append(
+                            $("#boardName").append(
                                 $("<option>").val(nameValue).text(nameValue)
                             );
                         });
